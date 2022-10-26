8dcbe83fe74dbb88ceb85fbf6666bc67a929d360
MD ASIF KABIR EMON <mdasifkabiremon@MDs-MacBook-Air.local>
2022-10-26 21:06:32 +0800

    Courses Added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 4000;
 // app.use(cors());
 
 const categoreis = require("./data/categories.json");
+const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("Courses API");
@@ -13,6 +14,26 @@ app.get("/", (req, res) => {
 
 app.get("/course-category", (req, res) => {
   res.send(categoreis);
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "01") {
+    res.send(courses);
+  } else {
+    const specificCategoryCourses = courses.filter((n) => n.category_id == id);
+    res.send(specificCategoryCourses);
+  }
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((n) => n._id == id);
+  res.send(selectedCourse);
 });
 
 app.listen(port, () => {
