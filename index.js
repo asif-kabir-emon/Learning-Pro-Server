759f07931961245ded0acc781ef90f8edc84e64b
MD ASIF KABIR EMON <mdasifkabiremon@MDs-MacBook-Air.local>
2022-10-27 12:50:04 +0800

    Added More data

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 const port = process.env.PORT || 4000;
 
-// app.use(cors());
+app.use(cors());
 
 const categoreis = require("./data/categories.json");
 const courses = require("./data/courses.json");
